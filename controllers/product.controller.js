3069e90d5a52745b37a9bca50fb840f52f62bd51
RajRawat02 <30665752+RajRawat02@users.noreply.github.com>
2019-11-25 17:52:13 +0530

    medium link at routes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -43,3 +43,5 @@ exports.product_delete = function(req,res){
     res.send('Deleted Successfully');
   });
 };
+
+//https://medium.com/createdd-notes/starting-with-authentication-a-tutorial-with-node-js-and-mongodb-25d524ca0359
